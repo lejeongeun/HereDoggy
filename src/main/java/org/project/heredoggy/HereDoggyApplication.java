8b5a883b668aba847353361335c30f83e4f92b9e
jeong eun <lje7370@gmail.com>
2025-05-14 20:34:45 +0900

    first_commit

---
+++ src/main/java/org/project/heredoggy/HereDoggyApplication.java
@@ -0,0 +1,13 @@
+package org.project.heredoggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereDoggyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HereDoggyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/heredoggy/HereDoggyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.heredoggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereDoggyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
