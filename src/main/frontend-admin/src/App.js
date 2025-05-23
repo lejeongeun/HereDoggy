f94fb2d84a99a03d614bfff194ff7edcd97c13f3
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2025-05-23 16:54:32 +0900

    fix:node-module

--- src/main/frontend-admin/src/App.js
+++ src/main/frontend-admin/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/hello')
+      .then((res) => setMessage(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>🛠️ 보호소 관리자 페이지</h1>
+      <p>{message}</p>
     </div>
   );
 }
