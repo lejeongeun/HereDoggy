8b11121dbdfbb4627b7342780ac62d67354e8493
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2025-05-24 02:56:17 +0900

    feat:member edit

--- src/main/java/org/project/heredoggy/user/member/controller/MemberController.java
+++ src/main/java/org/project/heredoggy/user/member/controller/MemberController.java
@@ -1,15 +1,17 @@
 package org.project.heredoggy.user.member.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.project.heredoggy.security.CustomUserDetails;
+import org.project.heredoggy.user.member.dto.request.MemberEditRequestDTO;
 import org.project.heredoggy.user.member.dto.response.MemberDetailResponseDTO;
 import org.project.heredoggy.user.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +20,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/profile")
-    public ResponseEntity<?> getMyDetail(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        if(userDetails == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않았습니다.");
-        }
+    public ResponseEntity<MemberDetailResponseDTO> getMyDetail(@AuthenticationPrincipal CustomUserDetails userDetails) {
         MemberDetailResponseDTO member = memberService.getMemberDetails(userDetails);
         return ResponseEntity.ok(member);
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<Map<String, String>> edit(@Valid @RequestBody MemberEditRequestDTO request, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        memberService.edit(request, userDetails);
+        return ResponseEntity.ok(Map.of("message","회원 수정 성공"));
     }
 }
--- src/main/java/org/project/heredoggy/user/member/service/MemberService.java
+++ src/main/java/org/project/heredoggy/user/member/service/MemberService.java
@@ -1,20 +1,25 @@
 package org.project.heredoggy.user.member.service;
 
-import global.exception.InactiveAccountException;
-import global.exception.UnauthorizedException;
+import global.exception.ConflictException;
+import global.exception.NotFoundException;
 import global.util.AuthUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.project.heredoggy.domain.postgresql.member.Member;
 import org.project.heredoggy.domain.postgresql.member.MemberRepository;
 import org.project.heredoggy.security.CustomUserDetails;
+import org.project.heredoggy.user.member.dto.request.MemberEditRequestDTO;
 import org.project.heredoggy.user.member.dto.response.MemberDetailResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
     public MemberDetailResponseDTO getMemberDetails(CustomUserDetails userDetails) {
-        Member member = AuthUtils.getValidMember(userDetails);
+        Long memberId = AuthUtils.getValidMember(userDetails).getId();
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException("회원 정보를 찾을 수 없습니다."));
 
         return MemberDetailResponseDTO.builder()
                 .id(member.getId())
@@ -30,5 +35,23 @@ public class MemberService {
                 .totalWalkDuration(member.getTotalWalkDuration())
                 .createdAt(member.getCreatedAt())
                 .build();
+    }
+
+    public void edit(@Valid MemberEditRequestDTO request, CustomUserDetails userDetails) {
+        Member member = AuthUtils.getValidMember(userDetails);
+
+        if(memberRepository.existsByNicknameAndIdNot(request.getNickname(), member.getId())) {
+            throw new ConflictException("이미 사용 중인 닉네임입니다.");
+        }
+        String fullAddress = String.format("(%s) %s %s",
+                request.getZipcode(),
+                request.getAddress1(),
+                request.getAddress2()
+        );
+        member.setName(request.getName());
+        member.setNickname(request.getNickname());
+        member.setPhone(request.getPhone());
+        member.setAddress(fullAddress);
+        memberRepository.save(member);
     }
 }
