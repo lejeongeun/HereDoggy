073e29d56d8114d7daa9063221018da01a26273c
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2025-05-26 15:00:20 +0900

    feat:shelterRequestResponse 보호소 생성 요청 승인 또는 거렂 git commit -m feat:shelterRequestResponse

--- src/main/java/org/project/heredoggy/systemAdmin/controller/AdminShelterRequestController.java
+++ src/main/java/org/project/heredoggy/systemAdmin/controller/AdminShelterRequestController.java
@@ -1,15 +1,43 @@
 package org.project.heredoggy.systemAdmin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.project.heredoggy.security.CustomUserDetails;
+import org.project.heredoggy.systemAdmin.dto.ShelterRequestResponseDTO;
 import org.project.heredoggy.systemAdmin.service.AdminShelterRequestService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin")
+@RequestMapping("/api/admin/shelter-requests")
 public class AdminShelterRequestController {
     private final AdminShelterRequestService requestService;
 
+    @GetMapping
+    public ResponseEntity<List<ShelterRequestResponseDTO>> getAllRequest(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<ShelterRequestResponseDTO> res = requestService.getAllRequest(userDetails);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/response")
+    public ResponseEntity<List<ShelterRequestResponseDTO>> getAllRequestResponse(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<ShelterRequestResponseDTO> res = requestService.getAllRequestResponse(userDetails);
+        return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/{request_id}/approve")
+    public ResponseEntity<Map<String, String>> approveRequest(@PathVariable("request_id") Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        requestService.approveRequest(id, userDetails);
+        return ResponseEntity.ok(Map.of("message","보호소 생성 요청이 승인 되었습니다."));
+    }
+
+    @PostMapping("/{request_id}/reject")
+    public ResponseEntity<Map<String, String>> rejectRequest(@PathVariable("request_id") Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        requestService.rejectRequest(id, userDetails);
+        return ResponseEntity.ok(Map.of("message","보호소 생성 요청이 거절 되었습니다."));
+    }
 }
