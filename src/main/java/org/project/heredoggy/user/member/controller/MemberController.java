9ef0fcf109a5ffe89ced48643d092f86099de14d
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2025-05-26 12:03:08 +0900

    fix:security members,shelters

--- src/main/java/org/project/heredoggy/user/member/controller/MemberController.java
+++ src/main/java/org/project/heredoggy/user/member/controller/MemberController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
 
