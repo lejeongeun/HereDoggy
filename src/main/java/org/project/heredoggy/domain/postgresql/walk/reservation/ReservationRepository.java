b1bfe33f1c6932420765d4326519944c8243e55c
jeong eun <lje7370@gmail.com>
2025-05-28 20:34:29 +0900

    feat:shelter_reservations_admin reservations_status member_cancel_request

--- src/main/java/org/project/heredoggy/domain/postgresql/walk/reservation/ReservationRepository.java
+++ src/main/java/org/project/heredoggy/domain/postgresql/walk/reservation/ReservationRepository.java
@@ -1,13 +1,17 @@
 package org.project.heredoggy.domain.postgresql.walk.reservation;
 
 import org.project.heredoggy.domain.postgresql.member.Member;
+import org.project.heredoggy.domain.postgresql.walk.walkOption.WalkOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByMember(Member member);
+
+    List<Reservation> findByShelterId(Long sheltersId);
+
+    boolean existsByMemberAndWalkOptionAndStatueIn(Member member, WalkOption walkOption, List<WalkReservationStatus> pending);
 }
